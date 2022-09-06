561783fcfc6b8815ced098d7286d935ad1f85af2
rubenperezudc <ruben.perez.jove@udc.es>
2022-09-06 20:32:21 +0200

    directory restructuration

--- ai_fingerprinting_tool/__main__.py
+++
@@ -1,4 +0,0 @@
-from ai_fingerprinting_tool import app
-
-if __name__ == '__main__':
-    app.run()
--- ai_fingerprinting_tool/conf.py
+++ ai_fingerprinting_tool/conf.py
@@ -3,7 +3,7 @@ import os
 SCANS = ['p0f','nmap']
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-PERSISTENCE_DIR = os.path.join(ROOT_DIR, 'persistence')
+PERSISTENCE_DIR = os.path.abspath(os.path.join(os.path.join(ROOT_DIR, os.pardir),'persistence'))
 
 PERSISTENCE_p0f_DIR = os.path.join(PERSISTENCE_DIR, 'p0f')
 p0f_ENCODERS = os.path.join(PERSISTENCE_p0f_DIR, 'p0f_encoders.joblib')
