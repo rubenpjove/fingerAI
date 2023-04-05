2bce65fe7236593773366dd36528043da1f16a1c
Rubén Pérez <ruben.perez.jove@udc.es>
2023-04-06 01:11:04 +0200

    Release v.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fingerai',
-    version='1.0',
+    version='1.1',
     author='rubenpjove',
     description='OS Fingerprinting Tool based on Artifial Intelligence',
     url='https://github.com/rubenpjove/fingerAI',
